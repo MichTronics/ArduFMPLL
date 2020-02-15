6703239c50a2fea7f9610267078656210dc105dc
Michel van Veen <mhvv76@gmail.com>
2020-02-15 01:58:06 +0100

    Some testing to get lock byte from the TSA5511.

--- TSA5511/TSA5511.ino
+++ TSA5511/TSA5511.ino
@@ -1,14 +1,16 @@
-////////////////////////////////////////
-//                                    //
-//       ArduFMPLL by MichTronics     //
-//                  v0.1              //
-//                                    //
-////////////////////////////////////////
+////////////////////////////////////////////////
+//                                            //
+//           ArduFMPLL by MichTronics         //
+//                      v0.1                  //
+//  Working with the DRFS6 v2.0 FM 6W TX Kit  //
+//                                            //
+////////////////////////////////////////////////
 
 #include <Wire.h>
 
 void setup()
 {
+  Serial.begin(9600);
   Wire.begin(); // join i2c bus
   pll_set_frequency(94500);
 }
@@ -48,9 +50,16 @@ void pll_set_frequency(long pllfreq) {
   Wire.write(0x20);
   Wire.endTransmission();
 
-  // Here we wanna read the TSA5511 for Lock Signal
-  delay(500); 
-
+  // Here we wanna read the TSA5511 for Lock Signal not working yet.
+  Wire.requestFrom(96, 1);
+  if ( Wire.available() >= 1 )
+  {
+    int result = Wire.read() ;
+    Serial.println( result, HEX );
+  }
+  
+  delay(500);
+  
   // byte begin transmitting DRFS6 v2.0
   Wire.beginTransmission(96);
   Wire.write(0xCE);
