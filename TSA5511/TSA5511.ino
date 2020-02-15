cf2a44e6f5ce2065a44347315478d17795eaf1d8
Michel van Veen <mhvv76@gmail.com>
2020-02-15 01:38:49 +0100

    Function is working for the DRFS6 v2.0 FM transmitter Lock and Transmitting.

--- TSA5511/TSA5511.ino
+++ TSA5511/TSA5511.ino
@@ -10,52 +10,50 @@
 void setup()
 {
   Wire.begin(); // join i2c bus
-  setfreq(); // Test function to set freq
+  pll_set_frequency(94500);
 }
 
 void loop()
 {
 
 }
-void setfreq(){
-  Wire.beginTransmission(96);
-  Wire.write(0xCE);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  delay(12);
-  pll_set_frequency(94500);
-  delay(27);
-  Wire.beginTransmission(96);
-  Wire.write(0xCE);
-  Wire.write(0x20);
-  Wire.endTransmission();
-  delay(115);
-}
 
 void pll_set_frequency(long pllfreq) {
-  char init[2];
   char data[2];
-  char end[2];
   pllfreq = pllfreq / 8;
   pllfreq = pllfreq / 1;
   pllfreq = pllfreq * 1000;
   pllfreq = pllfreq / 6250;
-  //init[0] = 0xCE;
-  //init[1] = 0x00;
   data[0] = (pllfreq & 0xFF00) >> 8;
   data[1] = pllfreq & 0x00FF;
-  //end[0] = 0xCE;
-  //end[1] = 0x20;
-  //Wire.beginTransmission(96);
-  //Wire.write(init, 2);
-  //Wire.endTransmission();
-  //delay(12);
+
+  // Init byte
+  Wire.beginTransmission(96);
+  Wire.write(0xCE);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  
+  delay(12);
+
+  // Frequentie byte
   Wire.beginTransmission(96);
   Wire.write(data, 2);
   Wire.endTransmission();
-  //delay(26);
-  //Wire.beginTransmission(96);
-  //Wire.write(end, 2);
-  //Wire.endTransmission();
-  //delay(115);
+  
+  delay(27);
+
+  // Lock byte
+  Wire.beginTransmission(96);
+  Wire.write(0xCE);
+  Wire.write(0x20);
+  Wire.endTransmission();
+
+  // Here we wanna read the TSA5511 for Lock Signal
+  delay(500); 
+
+  // byte begin transmitting DRFS6 v2.0
+  Wire.beginTransmission(96);
+  Wire.write(0xCE);
+  Wire.write(0x24);
+  Wire.endTransmission();
 }
